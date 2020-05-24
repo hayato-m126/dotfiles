d16c7a6ea3254ba39fda6d0114e6a3df5d851623
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2020-05-24 12:52:12 +0900

    feat: add tmux

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@
 ln -snfv `realpath ./HOME/.gitconfig` ${HOME}/.gitconfig
 ln -snfv `realpath ./HOME/.editorconfig` ${HOME}/.editorconfig
 ln -snfv `realpath ./HOME/.bashrc` ${HOME}/.bashrc
+ln -snfv `realpath ./HOME/.tmux.conf` ${HOME}/.tmux.conf
 ln -snfv `realpath ./HOME/.ssh/config` ${HOME}/.ssh/config
 ln -snfv `realpath ./HOME/.config/fish/fishfile` ${HOME}/.config/fish/fishfile
 ln -snfv `realpath ./HOME/.config/fish/conf.d/config.fish` ${HOME}/.config/fish/conf.d/config.fish
