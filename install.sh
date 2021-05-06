701780f5b5b86665f2a93de07544efae550e00ed
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2021-05-06 12:55:07 +0900

    feat: add English folder name setting

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+
 cd HOME
 for file in `find . -type f` ; do
+  # escape . and /, replace ./ to ""
   relative_path=`echo $file | sed s@\.\/@@`
   # echo ${HOME}/$relative_path
   ln -snfv `realpath $file` ${HOME}/$relative_path
