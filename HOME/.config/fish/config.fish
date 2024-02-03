ea65da47a6629298c389e91a001d87b52b1497a2
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2024-02-03 20:11:40 +0900

    feat: delete ros1 setting

--- HOME/.config/fish/config.fish
+++ HOME/.config/fish/config.fish
@@ -1,18 +1,10 @@
 # Set $SHELL for venv in VSCode
 set SHELL (which fish)
 
-# colcon
-#alias colcon='__colcon_find_workspace_dir > /dev/null && cd (__colcon_find_workspace_dir); command colcon'
-
 # ROS
 if set -q ROS_DISTRO
-  if [ "$ROS_VERSION" = "1" ]
-    set -q ROS_DISTRO && source /opt/ros/$ROS_DISTRO/share/rosbash/rosfish
-  else
-    register-python-argcomplete --shell fish ros2 | source
-  end
+  register-python-argcomplete --shell fish ros2 | source
 end
 
-# Created by `pipx` on 2021-04-15 00:28:02
 set PATH $PATH /home/hyt/.local/bin
 ~/.local/bin/mise activate fish | source
