c55d7086d15bc131cd98e12dc46ac396f0b9f6af
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2024-07-07 01:20:23 +0900

    feat: pnpm setting

--- HOME/.config/fish/config.fish
+++ HOME/.config/fish/config.fish
@@ -8,3 +8,10 @@ end
 
 set PATH $PATH /home/hyt/.local/bin
 ~/.local/bin/mise activate fish | source
+
+# pnpm
+set -gx PNPM_HOME "/home/hyt/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
