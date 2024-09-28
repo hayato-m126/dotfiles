393cdd80240c7b64577fff99ee3cc919e5894850
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2024-09-28 16:41:11 +0900

    feat: add alias
    Signed-off-by: Hayato Mizushima <hayato-m126@users.noreply.github.com>

--- HOME/.config/fish/config.fish
+++ HOME/.config/fish/config.fish
@@ -6,7 +6,10 @@ if set -q ROS_DISTRO
   register-python-argcomplete --shell fish ros2 | source
 end
 
-~/.local/bin/mise activate fish | source
+# mise
+if test -f ~/.local/bin/mise
+    ~/.local/bin/mise activate fish | source
+end
 
 # pnpm
 set -gx PNPM_HOME "/home/hyt/.local/share/pnpm"
@@ -14,3 +17,6 @@ if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
+
+# alias
+alias docker='podman'
