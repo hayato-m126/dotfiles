8564961df3a4c52729e0fe6bdd887af1b4717c33
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2025-10-13 15:37:40 +0900

    feat: support ros2 jazzy (#18)
    Signed-off-by: Hayato Mizushima <hayato-m126@users.noreply.github.com>

--- HOME/.ros/setup.sh
+++ HOME/.ros/setup.sh
@@ -5,9 +5,12 @@ export ROS_DOMAIN_ID=77
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 export RCUTILS_COLORIZED_OUTPUT=1
 export LD_LIBRARY_PATH="/usr/local/libtorch/lib:$LD_LIBRARY_PATH"
+export CYCLONEDDS_URI=file://$HOME/.ros/cyclonedds.xml
 
 # source
 if [ -d "/opt/ros/humble" ]; then
-  export CYCLONEDDS_URI=file://$HOME/.ros/cyclonedds_humble.xml
   source /opt/ros/humble/setup.bash
+fi
+if [ -d "/opt/ros/jazzy" ]; then
+  source /opt/ros/jazzy/setup.bash
 fi
