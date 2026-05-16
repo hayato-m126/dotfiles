c45c3cd69bfeea574625bcac54da861a2ac2c4cb
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2026-05-16 17:15:06 +0900

    feat: update ros setting
    Signed-off-by: Hayato Mizushima <hayato-m126@users.noreply.github.com>

--- HOME/.ros/setup.sh
+++ HOME/.ros/setup.sh
@@ -1,11 +1,19 @@
-# DDS
-ifconfig lo multicast
 export ROS_LOCALHOST_ONLY=1
 export ROS_DOMAIN_ID=77
-export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 export RCUTILS_COLORIZED_OUTPUT=1
-export LD_LIBRARY_PATH="/usr/local/libtorch/lib:$LD_LIBRARY_PATH"
 export CYCLONEDDS_URI=file://$HOME/.ros/cyclonedds.xml
+
+# DDS
+#ifconfig lo multicast
+
+# autoware ansible
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+export CCACHE_DIR="$HOME/.ccache"
+export CC="/usr/lib/ccache/gcc"
+export CXX="/usr/lib/ccache/g++"
+export CMAKE_PREFIX_PATH="/opt/acados:${CMAKE_PREFIX_PATH}"
+export ACADOS_SOURCE_DIR="/opt/acados"
+export LD_LIBRARY_PATH="/opt/acados/lib:${LD_LIBRARY_PATH}"
 
 # source
 if [ -d "/opt/ros/humble" ]; then
