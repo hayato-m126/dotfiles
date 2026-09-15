4e0d071a3eda87cc52b291f86c2d844aeaa6f398
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2026-09-15 19:37:34 +0900

    feat: add copilot cli in devcontainer

--- install.sh
+++ install.sh
@@ -38,4 +38,6 @@ if [ -f "/.dockerenv" ] || [ -n "$REMOTE_CONTAINERS" ] || [ -n "$DEVCONTAINER" ]
 
   git clone --recursive --depth 1 --shallow-submodules https://github.com/akinomyoga/ble.sh.git ~/.ble.sh
   make -C ~/.ble.sh install PREFIX=~/.local
+
+  curl -fsSL https://gh.io/copilot-install | bash
 fi
